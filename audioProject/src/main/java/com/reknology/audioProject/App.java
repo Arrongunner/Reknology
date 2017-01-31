bbd9271b1c10edad8dd854aeb19b1b48582ce312
Arrongunner <jake.laver@ntlworld.com>
2017-01-31 13:23:24 +0000

    Added media player

--- audioProject/src/main/java/com/reknology/audioProject/App.java
+++ audioProject/src/main/java/com/reknology/audioProject/App.java
@@ -1,13 +1,39 @@
 package com.reknology.audioProject;
 
-/**
- * Hello world!
- *
- */
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class App 
 {
+
+	private static Scanner scanner = new Scanner(System.in);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	consoleStart();        
+    }
+    public static void consoleStart(){
+		String file = scanner.next();
+		MediaPlayer mediaPlayer = new MediaPlayer(file);
+		try {
+			mediaPlayer.playSound();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		scanner.close();
     }
 }
